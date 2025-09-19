43cb496b9b32898d7a04d50e334fc809abba4738
Promise Emefile <promisecruzfrancis@gmail.com>
2025-09-19 13:28:01 +0100

    Fix: import By for Selenium elemen

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from openai import OpenAI
