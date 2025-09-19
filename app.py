68c128cb0d8de1968230ce8a0ba6662fee787741
Promise Emefile <promisecruzfrancis@gmail.com>
2025-09-19 13:12:23 +0100

    Remove webdriver_manager and fix driver setup for Streamlit Cloud

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
