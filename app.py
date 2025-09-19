29bc721cc5316c60a6cc58a7d86735d0269a653b
Promise Emefile <promisecruzfrancis@gmail.com>
2025-09-19 13:06:15 +0100

    Clean packages and app file updated

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def setup_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    chrome_options.binary_location = "/usr/bin/chromium-browser"
+    chrome_options.binary_location = "/usr/bin/chromium"
 
     return webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=chrome_options)
 
